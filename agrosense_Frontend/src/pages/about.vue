f084ae302a1475035ec1ab66e4cb1f2506bfa1d7
Adem <712020:772a8aed-436a-40c6-9bd7-d409648bee6f@bots.bitbucket.org>
2023-06-18 12:25:53 +0100

    fixed ai

--- agrosense_Frontend/src/pages/about.vue
+++ agrosense_Frontend/src/pages/about.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="Card">
-    About Agrosense
+  <div class="card">
+    <iframe src="https://adamdr.vercel.app/" frameborder="0" class=" h-screen w-full"></iframe>
   </div>
 </template>
 <script setup>
--- agrosense_Frontend/src/pages/settings.vue
+++ agrosense_Frontend/src/pages/settings.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="card">
     Settings
   </div>
 </template>
